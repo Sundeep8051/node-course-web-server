6aefd5d852331bb1fe474ea9f880ded7c8e8b0aa
Sundeep <sundeepm1@gmail.com>
2018-09-22 10:27:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var app = express();
+const hbs = require('hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partialViews');
+
+hbs.registerHelper('getCurrentYear',()=>{ return new Date().getFullYear()});
+
+hbs.registerHelper('toUpper',(text)=>{
+    return text.ToUpper();
+});
+app.set('view engine','hbs');
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        heading:'Home Page',
+        pageDetails:'Rendering Home page from handlebar package',
+        
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        heading:'About Page',
+        pageDetails: 'Rendering About page from handlebar package using "HBS.registerPartials" method'
+    });
+});
+
+app.get('/badRequest',(req,res)=>{
+    res.send({
+        errorMessage : "Bad Request"
+    });
+});
+
+
+
+app.listen(3000);
