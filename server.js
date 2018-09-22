f6957c6b04321d68fe7e0e9fc4dd60383ed38565
Sundeep <sundeepm1@gmail.com>
2018-09-22 11:11:25 +0530

    changes port to PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var app = express();
 const hbs = require('hbs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/partialViews');
 
